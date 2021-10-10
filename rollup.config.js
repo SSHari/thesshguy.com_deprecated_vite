4d635e3a6160c54816ed169123e9e4b0c52ce010
Sai Hari <thesshguy@gmail.com>
2021-10-10 18:50:52 -0400

    Set up ssh dev scripts

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+import {
+  createFileConfig,
+  createExternalDeps,
+} from "ssh-dev-scripts/src/rollup.config"
+export default createFileConfig({
+  external: createExternalDeps({ "@babel/runtime": "version" }),
+})
