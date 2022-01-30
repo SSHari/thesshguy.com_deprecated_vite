9bf9c8d9817c66cc1826b61fcac9bc619c0b3c07
SSHari <thesshguy@gmail.com>
2022-01-30 10:00:32 -0500

    feat: 🎸 remove gatsby files

--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import { CSSProperties } from 'react';
-
-export type CustomCSSProperties = CSSProperties & Record<`--${string}`, any>;
-
-export const css = (...classes: (string | undefined)[]) =>
-  classes.filter(Boolean).join(' ');
