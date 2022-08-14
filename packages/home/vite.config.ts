7a5ad529004a57b35b6670edf3e6da7f0b42bf61
SSHari <thesshguy@gmail.com>
2022-08-14 16:37:35 -0400

    feat: 🎸 add github action to deploy home page

--- packages/home/vite.config.ts
+++ packages/home/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
-      components: 'src/components',
-      utils: 'src/utils',
+      components: path.resolve(__dirname, 'src/components'),
+      utils: path.resolve(__dirname, 'src/utils'),
     },
   },
-});
+}));
